04be59169e90bfe2ef5b7dcf99e364f31bf69660
DemmyRodriguez <rodriguezdemmy@gmail.com>
2022-04-27 03:53:34 -0400

    Adding white space content to anchor tag

--- League-Blog/league-of-legends-app/src/components/Footer.js
+++ League-Blog/league-of-legends-app/src/components/Footer.js
@@ -2,9 +2,9 @@ function Footer() {
     return (
         <>
             <div className="footer">
-            <a className="linkedInLink" href="https://www.linkedin.com/in/demmy-rodriguez"></a>
+            <a className="linkedInLink" href="https://www.linkedin.com/in/demmy-rodriguez"> </a>
             <h6>© 2022 Demmy's Projects  |  All Rights Reserved</h6>  
-            <a className="githubLink" href="https://github.com/DemmyRodriguez"></a>
+            <a className="githubLink" href="https://github.com/DemmyRodriguez"> </a>
             </div>
         </>
     )
